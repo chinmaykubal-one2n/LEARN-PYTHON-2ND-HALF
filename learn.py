aef2d5862a79f8bd3c5cfbfdd8cef928ee1377aa
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-03-12 16:46:21 +0530

    new learnings added

--- learn.py
+++ learn.py
@@ -1,6 +1,6 @@
 # print("hello world")
 
-# import sys
+# import sys # (sys is a built-in module in python)
 # print(sys.version)
 
 # if (10 > 4):  # Indentation is important in Python
@@ -38,6 +38,37 @@
 
 # print_name()
 
-# Learning about python data types :- https://www.w3schools.com/python/python_datatypes.asp
+# Learning about python data types :- https://www.w3schools.com/python/python_datatypes.asp (check readme.md)
+# Learning about Stings :-  https://www.w3schools.com/python/python_strings.asp (check readme.md)
 
 
+# # Strings are Arrays
+# message = "Hello, World!"
+# print(message[7]) # Output: W
+
+# for x in "banana":
+#   print(x)
+
+
+# # Check if a certain phrase or character is present in a string
+# text = """Contrary to popular belief, Lorem Ipsum is not simply random text. 
+# It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+# Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+# looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+# and going through the cites of the word in classical literature, discovered the undoubtable source.
+# Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. 
+# This book is a treatise on the theory of ethics, very popular during the Renaissance. 
+# The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32."""
+
+# print("Lorem Ipsum" in text) # Output: True
+
+
+# text = "best things in life are free!"
+# if "free" in text:
+#     print("Yes, 'free' is present in the text")
+
+# text = "The best things in life are free!"
+# if "expensive" not in text:
+#   print("No, 'expensive' is NOT present.")
+
+# # Slicing (https://www.w3schools.com/python/python_strings_slicing.asp)
